ae66823227a0a825374103d73e935624630ac2e2
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2008-07-08 21:02:12 +0000

    release 1.1.6

---
+++ trunk/src/com/google/inject/testing/guiceberry/junit3/BasicJunit3Module.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.inject.testing.guiceberry.junit3;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
+import com.google.inject.testing.guiceberry.GuiceBerryEnv;
+import com.google.inject.testing.guiceberry.TestId;
+import com.google.inject.testing.guiceberry.TestScoped;
+
+import junit.framework.TestCase;
+
+/**
+ * This Module provides the three basic bindings required by 
+ * {@link GuiceBerryJunit3}, namely {@link TestId}, 
+ * {@link TestCase} and the {@link TestScoped} scope. Without these three
+ * bindinds, {@link GuiceBerryJunit3#setUp(TestCase)} will fail.
+
+ * <p>Thus, this module is all but required to be installed by all 
+ * {@link GuiceBerryEnv}s using JUnit3. 
+ * 
+ * <p>The only alternative would be to provide the same bindings through some
+ * other means.
+ * 
+ * @see GuiceBerryEnv
+ * 
+ * @author Luiz-Otavio Zorzella
+ * @author Danka Karwanska
+ */
+public class BasicJunit3Module extends AbstractModule {
+    
+      @Override
+      public void configure() {
+        final JunitTestScope testScope = new JunitTestScope();
+        bindScope(TestScoped.class, testScope);
+        bind(TestId.class).toProvider(new Provider<TestId>() {
+          public TestId get() {
+            TestCase testClass = GuiceBerryJunit3.getActualTestCase(); 
+            return new TestId(testClass);
+          } 
+        }).in(TestScoped.class);
+        bind(TestCase.class).toProvider(new Provider<TestCase>() {
+          public TestCase get() {
+            return GuiceBerryJunit3.getActualTestCase();
+          }
+        }).in(TestScoped.class);
+      }
+}
---
+++ trunk/test/com/google/inject/testing/guiceberry/junit3/MutableSingletonScopeTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.inject.testing.guiceberry.junit3;
+
+import com.google.common.testing.junit3.TearDownTestCase;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.ScopeAnnotation;
+import com.google.inject.testing.guiceberry.MutableSingletonScope;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Tests the {@link MutableSingletonScope} class.
+ * 
+ * @author Luiz-Otavio Zorzella
+ * @author Danka Karwanska
+ */
+public class MutableSingletonScopeTest extends TearDownTestCase  {
+  
+  public void testBarServiceIsTheSameInstanceInMutableSingletonScope() {
+    
+    Something something = new Something();
+    SomethingElse somethingElse = new SomethingElse();
+    
+    Injector injector = Guice.createInjector(new FooModule());
+    assertNull(somethingElse.barService);
+    assertNull(something.barService);
+    injector.injectMembers(somethingElse);
+    injector.injectMembers(something);
+    assertSame(something.barService, somethingElse.barService);
+  }
+  
+  public void testBarServiceIsDifferentInstanceAfterClearMutableSingletonScope() {
+
+    Something something = new Something();
+    SomethingElse somethingElse = new SomethingElse();
+    
+    Injector injector = Guice.createInjector(new FooModule());
+    assertNull(something.barService);
+    assertNull(something.barService);
+    injector.injectMembers(somethingElse);
+    injector.getInstance(MutableSingletonScope.class).clear();
+    injector.injectMembers(something);
+    assertNotSame(something.barService, somethingElse.barService);
+  }
+  
+  private static class FooModule extends AbstractModule {
+    @Override
+    public void configure() {
+      MutableSingletonScope mutableSingletonScope = new MutableSingletonScope();
+      bind(MutableSingletonScope.class).toInstance(mutableSingletonScope);
+      bindScope(MutableSingletonScoped.class, mutableSingletonScope);
+      bind(BarService.class).in(MutableSingletonScoped.class);
+    }
+  }
+  
+  @Target(ElementType.TYPE)
+  @Retention(RetentionPolicy.RUNTIME)
+  @ScopeAnnotation
+  private @interface MutableSingletonScoped {}
+     
+  private static class BarService { }
+  
+  private static class Something {
+    @Inject BarService barService;
+  }
+  
+  private static class SomethingElse {
+    @Inject BarService barService;
+  }
+}
