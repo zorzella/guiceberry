cbd481c12f8be0a98ff8dfffb344c318e38ee1c2
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2009-06-03 23:12:06 +0000

    PageObjects

--- doc/tutorial/test/com/google/inject/testing/guiceberry/tutorial_1_server/WelcomeTestPage.java
+++ doc/tutorial/test/com/google/inject/testing/guiceberry/tutorial_1_server/WelcomeTestPage.java
@@ -1,6 +1,3 @@
-// Copyright 2008 Google Inc.  All Rights Reserved.
-// Copyright 2009 Google Inc. All Rights Reserved.
-
 package com.google.inject.testing.guiceberry.tutorial_1_server;
 
 import com.google.inject.Inject;
