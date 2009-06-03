2062bbd1a9846d21ad15744bf91225934779cfd4
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2009-06-03 23:11:50 +0000

    PageObjects

--- trunk/doc/tutorial/test/com/google/inject/testing/guiceberry/tutorial_1_server/Example1PageObjectsTest.java
+++ trunk/doc/tutorial/test/com/google/inject/testing/guiceberry/tutorial_1_server/Example1PageObjectsTest.java
@@ -3,11 +3,7 @@ package com.google.inject.testing.guiceberry.tutorial_1_server;
 import com.google.inject.Inject;
 import com.google.inject.testing.guiceberry.GuiceBerryEnv;
 import com.google.inject.testing.guiceberry.junit3.GuiceBerryJunit3TestCase;
-import com.google.inject.testing.guiceberry.tutorial_1_server.RegularPetStoreAt8080Env.PortNumber;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 @GuiceBerryEnv(Tutorial1Envs.REGULAR_PET_STORE_AT_8080_ENV)
 public class Example1PageObjectsTest extends GuiceBerryJunit3TestCase {
@@ -23,27 +19,5 @@ public class Example1PageObjectsTest extends GuiceBerryJunit3TestCase {
   public void testMyServletTitle() {
     welcomeTestPage.goTo();
     welcomeTestPage.assertTitle();
-  }
-  
-  public static final class WelcomeTestPage {
-    @Inject
-    WebDriver driver;
-    
-    @Inject
-    @PortNumber
-    int portNumber;
-    
-    public void goTo() {
-      driver.get("http://localhost:" + portNumber);
-    }
-    
-    public void assertWelcomeMessage() {
-      WebElement element = driver.findElement(By.xpath("//div[@id='welcome']"));
-      assertEquals("Welcome!", element.getText());
-    }
-    
-    public void assertTitle() {
-      assertEquals("Welcome to the pet store", driver.getTitle());
-    }
   }
 }
---
+++ trunk/doc/tutorial/test/com/google/inject/testing/guiceberry/tutorial_1_server/WelcomeTestPage.java
@@ -0,0 +1,33 @@
+// Copyright 2008 Google Inc.  All Rights Reserved.
+// Copyright 2009 Google Inc. All Rights Reserved.
+
+package com.google.inject.testing.guiceberry.tutorial_1_server;
+
+import com.google.inject.Inject;
+import com.google.inject.testing.guiceberry.tutorial_1_server.RegularPetStoreAt8080Env.PortNumber;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public final class WelcomeTestPage {
+  @Inject
+  WebDriver driver;
+  
+  @Inject
+  @PortNumber
+  int portNumber;
+  
+  public void goTo() {
+    driver.get("http://localhost:" + portNumber);
+  }
+  
+  public void assertWelcomeMessage() {
+    WebElement element = driver.findElement(By.xpath("//div[@id='welcome']"));
+    Example1PageObjectsTest.assertEquals("Welcome!", element.getText());
+  }
+  
+  public void assertTitle() {
+    Example1PageObjectsTest.assertEquals("Welcome to the pet store", driver.getTitle());
+  }
+}
