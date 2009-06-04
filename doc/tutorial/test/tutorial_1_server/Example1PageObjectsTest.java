a0e3f09815c4dced3b0635a4a033f98e7f65f7b1
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2009-06-04 18:36:53 +0000

    Moved tutorial to a top-level pacakge

---
+++ doc/tutorial/test/tutorial_1_server/Example1PageObjectsTest.java
@@ -0,0 +1,23 @@
+package tutorial_1_server;
+
+import com.google.inject.Inject;
+import com.google.inject.testing.guiceberry.GuiceBerryEnv;
+import com.google.inject.testing.guiceberry.junit3.GuiceBerryJunit3TestCase;
+
+
+@GuiceBerryEnv(Tutorial1Envs.REGULAR_PET_STORE_AT_8080_ENV)
+public class Example1PageObjectsTest extends GuiceBerryJunit3TestCase {
+
+  @Inject
+  WelcomeTestPage welcomeTestPage;
+  
+  public void testMyServletDiv() {
+    welcomeTestPage.goTo();
+    welcomeTestPage.assertWelcomeMessage();
+  }
+
+  public void testMyServletTitle() {
+    welcomeTestPage.goTo();
+    welcomeTestPage.assertTitle();
+  }
+}
---
+++ doc/tutorial/test/tutorial_2_advanced/Tutorial2Envs.java
@@ -0,0 +1,10 @@
+package tutorial_2_advanced;
+
+public interface Tutorial2Envs {
+
+  String PACKAGE = "com.google.inject.testing.guiceberry.tutorial_2_advanced.";
+  
+  String EXAMPLE_5 = PACKAGE + 
+    "Example5Remapper$FakeGuiceBerryEnv";
+
+}
