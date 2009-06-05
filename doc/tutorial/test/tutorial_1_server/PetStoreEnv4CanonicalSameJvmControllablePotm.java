f098369e6ff67617174e06a4a367770b40f30739
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2009-06-05 15:42:31 +0000

    Broken controllable injection examples added

--- doc/tutorial/test/tutorial_1_server/PetStoreEnv4CanonicalSameJvmControllablePotm.java
+++ doc/tutorial/test/tutorial_1_server/PetStoreEnv4CanonicalSameJvmControllablePotm.java
@@ -1,8 +1,11 @@
 package tutorial_1_server;
 
+import com.google.inject.Key;
 import com.google.inject.Module;
 import com.google.inject.Provides;
+import com.google.inject.Scopes;
 import com.google.inject.Singleton;
+import com.google.inject.internal.Maps;
 import com.google.inject.testing.guiceberry.TestId;
 import com.google.inject.testing.guiceberry.junit3.GuiceBerryJunit3Env;
 
@@ -11,9 +14,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import tutorial_1_server.prod.MyPetStoreServer;
+import tutorial_1_server.prod.PetOfTheMonth;
 import tutorial_1_server.prod.PortNumber;
 
 import java.util.List;
+import java.util.Map;
 
 public final class PetStoreEnv4CanonicalSameJvmControllablePotm extends GuiceBerryJunit3Env {
   
@@ -34,11 +39,11 @@ public final class PetStoreEnv4CanonicalSameJvmControllablePotm extends GuiceBer
   
   @Provides
   @Singleton
-  MyPetStoreServer startServer() {
+  MyPetStoreServer startServer(final ModuleRewriter moduleRewriter) {
     MyPetStoreServer result = new MyPetStoreServer(8080) {
       @Override
       protected List<? extends Module> getModules() {
-        return rewrite(super.getModules());
+        return rewrite(moduleRewriter, super.getModules());
       }
 
     };
@@ -46,12 +51,100 @@ public final class PetStoreEnv4CanonicalSameJvmControllablePotm extends GuiceBer
     return result;
   }
 
-  // class LocalJvmControllableProvider<T> implements Provider<T> {}
+  public interface InjectionControllerRegistry {
+
+    <T> InjectionControllerI<T> get(Class<T> clazz);
+    <T> InjectionControllerI<T> get(Key<T> clazz);
+  }
   
-  private static List<? extends Module> rewrite(List<? extends Module> modules) {
-    // return new ModuleRewriter()
-    //   .rewrite(PetOfTheMonth.class, LocalJvmControllableProdider.class);
-    //   .build();
-    return modules;
+  public interface InjectionControllerI<T> {
+    void substituteFor(T override);
+  }
+  
+  public static final class ModuleRewriter {
+
+    private List<? extends Module> modules;
+    // Each entry in the map is of the same "?" as its key
+    private final Map<Class<?>, Controller<?>> map = Maps.newHashMap();
+    
+    public InjectionControllerRegistry getInjectionController(final TestId testId) {
+      return new InjectionControllerRegistry() {
+        
+        public <T> InjectionControllerI<T> get(final Class<T> clazz) {
+          
+          return new InjectionControllerI<T>() {
+
+            public void substituteFor(T override) {
+//              map.get(clazz).setOverride(testId, override);
+            }
+          };
+        }
+
+        public <T> InjectionControllerI<T> get(Key<T> clazz) {
+          return null;
+        }
+        
+      };
+    }
+
+    public ModuleRewriter forModules(List<? extends Module> modules) {
+      if (modules != null) {
+        throw new IllegalStateException();
+      }
+      this.modules = modules;
+      return this;
+    }
+
+    public <T> ModuleRewriter rewrite(Class<T> clazz, 
+        Controller<T> controller) {
+      map.put(clazz, controller);
+      return this;
+    }
+
+    public List<? extends Module> build() {
+      return modules;
+    }
+  }
+  
+  @Override
+  protected void configure() {
+    super.configure();
+    bind(ModuleRewriter.class).in(Scopes.SINGLETON);
+  }
+  
+  @Provides
+  InjectionControllerRegistry getInjectionController(TestId testId, ModuleRewriter moduleRewriter) {
+    return moduleRewriter.getInjectionController(testId);
+  }
+  
+  public interface Controller<T> {
+    
+    public T getOverride(TestId testId);
+    public void setOverride(TestId testId, T override);
+    
+  }
+  
+  static class LocalJvmControllableProvider<T> implements Controller<T> {
+
+    Map<TestId,T> map = Maps.newHashMap();
+    
+    public T getOverride(TestId testId) {
+      return map.get(testId); 
+    }
+
+    public void setOverride(TestId testId, T override) {
+      map.put(testId, override);
+    }
+  }
+  
+  private static List<? extends Module> rewrite(
+      ModuleRewriter moduleRewriter, 
+      List<? extends Module> modules) {
+     List<? extends Module> result = moduleRewriter
+       .forModules(modules)
+       .rewrite(PetOfTheMonth.class, 
+           new LocalJvmControllableProvider<PetOfTheMonth>())
+       .build();
+    return result;
   }
 }
