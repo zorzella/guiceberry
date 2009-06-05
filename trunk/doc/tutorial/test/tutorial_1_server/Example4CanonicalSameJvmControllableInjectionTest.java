9857f858bba41e8634eaf989a25aefcf940a523d
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2009-06-05 15:42:31 +0000

    Broken controllable injection examples added

--- trunk/doc/tutorial/test/tutorial_1_server/Example4CanonicalSameJvmControllableInjectionTest.java
+++ trunk/doc/tutorial/test/tutorial_1_server/Example4CanonicalSameJvmControllableInjectionTest.java
@@ -2,9 +2,9 @@ package tutorial_1_server;
 
 import com.google.inject.Inject;
 import com.google.inject.testing.guiceberry.GuiceBerryEnv;
-import com.google.inject.testing.guiceberry.controllable.InjectionController;
 import com.google.inject.testing.guiceberry.junit3.GuiceBerryJunit3TestCase;
 
+import tutorial_1_server.PetStoreEnv4CanonicalSameJvmControllablePotm.InjectionControllerRegistry;
 import tutorial_1_server.prod.PetOfTheMonth;
 
 @GuiceBerryEnv(Tutorial1Envs.PET_STORE_ENV_4_CANONICAL_SAME_JVM_CONTROLLABLE_POTM)
@@ -14,20 +14,20 @@ public class Example4CanonicalSameJvmControllableInjectionTest extends GuiceBerr
   WelcomeTestPage welcomeTestPage;
   
   @Inject
-  private InjectionController injectionController;
+  private InjectionControllerRegistry injectionControllerRegistry;
 
   // TODO: tests are failing -- fix the Env to actually rewrite the bindings.
   
   public void testDogAsPotm() {
     PetOfTheMonth expected = PetOfTheMonth.DOG;
-    injectionController.substitute(PetOfTheMonth.class, expected);
+    injectionControllerRegistry.get(PetOfTheMonth.class).substituteFor(expected);
     welcomeTestPage.goTo();
     welcomeTestPage.assertPetOfTheMonth(expected);
   }
 
   public void testCatAsPotm() {
     PetOfTheMonth expected = PetOfTheMonth.CAT;
-  injectionController.substitute(PetOfTheMonth.class, expected);
+  injectionControllerRegistry.get(PetOfTheMonth.class).substituteFor(expected);
   welcomeTestPage.goTo();
   welcomeTestPage.assertPetOfTheMonth(expected);
   }
