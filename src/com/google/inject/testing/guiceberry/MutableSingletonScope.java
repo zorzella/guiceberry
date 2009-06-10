4a45b7c3b7ddc675c4020553d4ab9898edaec9ed
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2009-06-10 15:36:41 +0000

    Move MutableSingletonScope to util subpackage

--- src/com/google/inject/testing/guiceberry/MutableSingletonScope.java
+++ src/com/google/inject/testing/guiceberry/MutableSingletonScope.java
@@ -16,51 +16,11 @@
 
 package com.google.inject.testing.guiceberry;
 
-import com.google.inject.Key;
-import com.google.inject.Provider;
-import com.google.inject.Scope;
-
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * Convenience class to create a {@link Scope} for mutable/replaceable 
- * singletons, i.e. those classes that, at any given moment in time, are 
- * singletons, but this singleton instance might be replaced.
- * 
- * @see #clear()
+ * @deprecated use {@link com.google.inject.testing.guiceberry.util.MutableSingletonScope}
  * 
  * @author zorzella
  */
-public class MutableSingletonScope implements Scope {
-
-  private final Map<Key<?>, Object> backingMap = new HashMap<Key<?>, Object>();
-
-  /**
-   * Clears all the cached instances. After this method is called, any 
-   * "singleton" bound to this scope that had already been created will be
-   * created again next time it gets injected.
-   */
-  public void clear() {
-    backingMap.clear();
-  }
-  
-  @SuppressWarnings("unchecked")  
-  public synchronized <T> Provider<T> scope(
-      final Key<T> key, final Provider<T> unscoped) {
-
-    return new Provider<T>() {
-      public T get() {
-
-        Object o = backingMap.get(key);
-
-        if (o == null) {
-          o = unscoped.get();
-          backingMap.put(key, o);
-        }
-        return (T) o;
-      }
-    };
-  }
-}
+@Deprecated
+public class MutableSingletonScope extends com.google.inject.testing.guiceberry.util.MutableSingletonScope {}
   
