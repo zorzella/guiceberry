f3d4ea082f29ec5dc493f1823387f47c5c5f3c3b
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2010-07-10 05:05:18 +0000

    JUnit3+TearDownTestCase support. Tutorial branch for that

---
+++ trunk/doc/tutorial/test/junit3_tdtc/tutorial_1_server/Example2ManualControlledInjectionTest.java
@@ -0,0 +1,53 @@
+package junit3_tdtc.tutorial_1_server;
+
+import com.google.common.testing.TearDown;
+import com.google.common.testing.TearDownAccepter;
+import com.google.common.testing.junit3.TearDownTestCase;
+import com.google.guiceberry.junit3.TearDownGuiceBerry;
+import com.google.inject.Inject;
+
+import junit3_tdtc.tutorial_1_server.PetStoreEnv2GlobalStaticControllablePotm.PetStoreModuleWithGlobalStaticOverride;
+import junit3_tdtc.tutorial_1_server.prod.PetOfTheMonth;
+
+public class Example2ManualControlledInjectionTest extends TearDownTestCase {
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    TearDownGuiceBerry.setup(this, PetStoreEnv2GlobalStaticControllablePotm.class);
+  }
+
+  @Inject
+  TearDownAccepter tearDownAccepter;
+  
+  @Inject
+  WelcomeTestPage welcomeTestPage;
+
+  public void testDogAsPotm() {
+    PetOfTheMonth expected = PetOfTheMonth.DOG;
+    PetStoreModuleWithGlobalStaticOverride.override = expected;
+    // register a tearDown, so that at the end of the test, 
+    // the override is set to null again
+    tearDownAccepter.addTearDown(new TearDown() {
+      public void tearDown() {
+        PetStoreModuleWithGlobalStaticOverride.override = null;
+      }
+    });
+    welcomeTestPage.goTo();
+    welcomeTestPage.assertPetOfTheMonth(expected);
+  }
+
+  public void testCatAsPotm() {
+    PetOfTheMonth expected = PetOfTheMonth.CAT;
+    PetStoreModuleWithGlobalStaticOverride.override = expected;
+    // register a tearDown, so that at the end of the test, 
+    // the override is set to null again
+    tearDownAccepter.addTearDown(new TearDown() {
+      public void tearDown() {
+        PetStoreModuleWithGlobalStaticOverride.override = null;
+      }
+    });
+    welcomeTestPage.goTo();
+    welcomeTestPage.assertPetOfTheMonth(expected);
+  }
+}
