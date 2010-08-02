cc3ef1850c70baa5c5bf8ef6ea5749a053d0a335
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2010-08-02 01:10:14 +0000

    Tuturial cleanup

--- doc/tutorial/test/tutorial_1_server/prod/PetOfTheMonth.java
+++ doc/tutorial/test/tutorial_1_server/prod/PetOfTheMonth.java
@@ -1,16 +1,19 @@
 package tutorial_1_server.prod;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import com.google.inject.BindingAnnotation;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Denotes which {@link Pet} is the special pet to be advertised this month.
  */
-@Retention(RetentionPolicy.RUNTIME) 
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER}) 
+@Retention(RUNTIME) 
+@Target({FIELD, METHOD, PARAMETER}) 
 @BindingAnnotation
 public @interface PetOfTheMonth {}
