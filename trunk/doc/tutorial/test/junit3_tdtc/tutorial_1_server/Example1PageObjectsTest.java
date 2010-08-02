299f9b7d63e98992007f21e4d9d4aaed0bae8554
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2010-08-02 15:52:46 +0000

    Tuturial cleanup

--- trunk/doc/tutorial/test/junit3_tdtc/tutorial_1_server/Example1PageObjectsTest.java
+++ trunk/doc/tutorial/test/junit3_tdtc/tutorial_1_server/Example1PageObjectsTest.java
@@ -18,13 +18,13 @@ public class Example1PageObjectsTest extends TearDownTestCase {
   @Inject
   WelcomeTestPage welcomeTestPage;
 
-  public void testMyServletDiv() {
+  public void testPetStoreWelcomeMessage() {
     welcomeTestPage
       .goTo()
       .assertWelcomeMessageIs("Welcome!");
   }
 
-  public void testMyServletTitle() {
+  public void testPetStoreTitle() {
     welcomeTestPage
       .goTo()
       .assertTitleIs("Welcome to the pet store");
