3f10b4027e39bc2906c9970802b11189153d3dfc
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2010-08-03 00:11:15 +0000

    Add TestNG support, and the tutorial Example0

---
+++ trunk/doc/tutorial/test/testng/tutorial_0_basic/Example0HelloWorldTest.java
@@ -0,0 +1,37 @@
+package testng.tutorial_0_basic;
+
+import com.google.common.testing.TearDown;
+import com.google.guiceberry.GuiceBerryModule;
+import com.google.guiceberry.testng.TestNgGuiceBerry;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
+
+public class Example0HelloWorldTest {
+
+  private TearDown toTearDown;
+  
+  @BeforeMethod
+  public void setUp(Method m) {
+    // Make this the call to ManualTearDownGuiceBerry.setUp as early as possible
+    toTearDown = TestNgGuiceBerry.setUp(this, m, Env.class);
+  }
+  
+  @AfterMethod
+  public void tearDown() throws Exception {
+    // Make this the call to ManualTearDownGuiceBerry.tearDown Up as late as possible
+    toTearDown.tearDown();
+  }
+
+  @Test
+  public void testNothing() throws Exception {
+    Assert.assertTrue(true);
+  }
+
+  public static final class Env extends GuiceBerryModule {}
+}
+
