9adedcfd0d2ccbb911753b689f670e3ac9c96e66
zorzella <zorzella@221315ec-b33e-0410-b5a4-5515197fefce>
2011-12-07 18:57:16 +0000

    Fixed package-info javadoc link from draft guiceberry tutorial to correct one

--- trunk/doc/tutorial/test-guiceberry-2/guiceberry2_junit3/tutorial_0_basic/package-info.java
+++ trunk/doc/tutorial/test-guiceberry-2/guiceberry2_junit3/tutorial_0_basic/package-info.java
@@ -2,5 +2,5 @@ package guiceberry2_junit3.tutorial_0_basic;
 
 /**
  * The documentation for this tutorial can be found
- * at https://docs.google.com/a/google.com/Doc?id=chsf26tp_124jp93wgg
+ * at http://docs.google.com/View?id=dddf9mmq_29s3s79fcv
  */
